6d92b8318297dde6fe78ff46c19c793bbcdadec1
Javaughn Pryce <javanpryce1@gmail.com>
2024-04-24 12:22:20 -0400

    added redirect to  https://proofbuddy.cci.drexel.edu/ if Natural Deduction is selected

--- client/src/pages/NaturalDeductionPropositionalLogic.js
+++ client/src/pages/NaturalDeductionPropositionalLogic.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import MainLayout from '../layouts/MainLayout';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/esm/Button';
-import Col from 'react-bootstrap/Col';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import MainLayout from "../layouts/MainLayout";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/esm/Button";
+import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
 
 const NaturalDeductionPropositionalLogic = () => {
+  useEffect(() => {
+    window.location.replace("https://proofbuddy.cci.drexel.edu/");
+  }, []);
 
   return (
     <MainLayout>
-      <Container className='natural-deduction-propositional-logic-container'>
-        <Col className='text-center'>
+      <Container className="natural-deduction-propositional-logic-container">
+        <Col className="text-center">
           <h2>Natural Deduction: Propositional Logic</h2>
         </Col>
 
         <Col>
-          <Link to='/'>
+          <Link to="/">
             <Button>Back</Button>
           </Link>
         </Col>
-
       </Container>
     </MainLayout>
   );
