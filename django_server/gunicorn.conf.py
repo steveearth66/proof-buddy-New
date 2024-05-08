993f40e4022fefd869d980149c289dc6e5fabd44
Javaughn Pryce <javanpryce1@gmail.com>
2024-05-08 19:29:35 -0400

    update logs

--- django_server/gunicorn.conf.py
+++ django_server/gunicorn.conf.py
@@ -2,7 +2,7 @@ bind = "0.0.0.0:8000"
 workers = 3
 timeout = 120
 
-accesslog = '/var/log/gunicorn/access.log'
-errorlog = '/var/log/gunicorn/error.log'
+accesslog = '/var/log/gunicorn.access.log'
+errorlog = '/var/log/gunicorn.error.log'
 loglevel = 'info'
 capture_output = True
