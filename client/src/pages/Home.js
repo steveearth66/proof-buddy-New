02b9515f75138987cc649953cd2c1de162862cb6
Javaughn Pryce <javanpryce1@gmail.com>
2024-05-20 20:16:07 -0400

    add option to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -22,44 +22,73 @@ const Home = () => {
 
   return (
     <MainLayout>
-      <Container className='home-container'>
+      <Container className="home-container">
         <Row>
           <Col md={6} className="dragon-section">
-            <Image src={dragon} alt='Drexel dragon' fluid />
+            <Image src={dragon} alt="Drexel dragon" fluid />
           </Col>
           <Col md={6} className="text-section">
             <h1>Proof Buddy</h1>
             {user ? (
               <>
-                <p>Welcome, {user.username} to your {user.is_student === true ? 'student' : 'instructor'} account!</p>
+                <p>
+                  Welcome, {user.username} to your{" "}
+                  {user.is_student === true ? "student" : "instructor"} account!
+                </p>
                 <p>Let's begin working on some proofs!</p>
                 <br></br>
                 <Form>
                   <Form.Group>
                     <Form.Floating>
-                      <FloatingLabel controlId="proofType" label="Type of Proof">
+                      <FloatingLabel
+                        controlId="proofType"
+                        label="Type of Proof"
+                      >
                         <Form.Select
                           aria-label="Floating label proof type select"
-                          id='proofType'
-                          onChange={e => { setProofType(e.target.value); }}>
-                          <option value='/er-racket'>Equational Reasoning: Racket</option>
-                          <option value='/NaturalDeductionPropositionalLogic'>Natural Deduction: Propositional Logic</option>
-                          <option value='/NaturalDeductionFirstOrderLogic'>Natural Deduction: First Order Logic</option>
+                          id="proofType"
+                          onChange={(e) => {
+                            setProofType(e.target.value);
+                          }}
+                        >
+                          <option value="/er-racket">
+                            Equational Reasoning: Racket
+                          </option>
+                          <option value="/induction-racket">
+                            Equational Reasoning: Induction
+                          </option>
+                          <option value="/NaturalDeductionPropositionalLogic">
+                            Natural Deduction: Propositional Logic
+                          </option>
+                          <option value="/NaturalDeductionFirstOrderLogic">
+                            Natural Deduction: First Order Logic
+                          </option>
                         </Form.Select>
                       </FloatingLabel>
                     </Form.Floating>
                   </Form.Group>
 
-                  <div className='button-wrap'>
-                    <Button as={Link} className='orange-btn' to={proofType}>Let's Begin</Button>
+                  <div className="button-wrap">
+                    <Button as={Link} className="orange-btn" to={proofType}>
+                      Let's Begin
+                    </Button>
                   </div>
                 </Form>
               </>
             ) : (
               <>
-                <p>Welcome to the platform for verifying mathematical proofs using Truth-Functional Logic (TFL) and First Order Logic (FOL).</p>
-                <p><Link to='/login'>Log in</Link> to begin working on some proofs!</p>
-                <p>Don't have an account? <Link to="/signup">Sign up</Link> now!</p>
+                <p>
+                  Welcome to the platform for verifying mathematical proofs
+                  using Truth-Functional Logic (TFL) and First Order Logic
+                  (FOL).
+                </p>
+                <p>
+                  <Link to="/login">Log in</Link> to begin working on some
+                  proofs!
+                </p>
+                <p>
+                  Don't have an account? <Link to="/signup">Sign up</Link> now!
+                </p>
               </>
             )}
           </Col>
