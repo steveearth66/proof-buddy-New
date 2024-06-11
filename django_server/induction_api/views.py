48bc99250c87397ad399acfc119cc7e850979690
Andy Chek <acc374@drexel.edu>
2024-06-11 16:42:10 -0400

    backend mostly set up

--- django_server/induction_api/views.py
+++ django_server/induction_api/views.py
@@ -22,21 +22,23 @@ def start_induction_proof(request):
 
     proof['current_proof'] = currentProof
 
-    if not (json_data['inductionVariable'] in json_data['anchorGoal'] or json_data['leapGoal']):
-        errors = "could not find IVar in either Induction goals"
+    if not (json_data['inductionVariable'] in json_data['lhsGoal'] or json_data['rhsGoal']):
+        errors = ["could not find IVar in either Induction goals"]
         proof['isValid'] = False
+        return Response({"isValid": is_valid, "errors": errors}, status=status.HTTP_200_OK)
     
     if (json_data['leapVariable'] in proof["lhs_leap"].ruleSet.keys()) or \
        (json_data['leapVariable'] in proof["rhs_leap"].ruleSet.keys()) or \
        (json_data['leapVariable'] in proof["lhs_anchor"].ruleSet.keys()) or \
        (json_data['leapVariable'] in proof["rhs_anchor"].ruleSet.keys()):
-        errors = "LVar found in the Ruleset, please choose another variable"
+        errors = ["LVar found in the Ruleset, please choose another variable"]
         proof['isValid'] = False
+        return Response({"isValid": is_valid, "errors": errors}, status=status.HTTP_200_OK)
         
     anchorLHS = json_data['lhsGoal'].replace(json_data['inductionVariable'], json_data['inductionValue'])
     anchorRHS = json_data['rhsGoal'].replace(json_data['inductionVariable'], json_data['inductionValue'])
-    leapLHS = json_data['lhsGoal'].replace(json_data['inductionVariable'], f'(+ 1 {json_data['inductionVariable']})')
-    leapRHS = json_data['rhsGoal'].replace(json_data['inductionVariable'], f'(+ 1 {json_data['inductionVariable']})')
+    leapLHS = json_data['lhsGoal'].replace(json_data['inductionVariable'], f"(+ 1 {json_data['inductionVariable']})")
+    leapRHS = json_data['rhsGoal'].replace(json_data['inductionVariable'], f"(+ 1 {json_data['inductionVariable']})")
 
     proof['lhs_anchor'].addProofLine(anchorLHS)
     proof['rhs_anchor'].addProofLine(anchorRHS)
@@ -100,6 +102,44 @@ def add_definitions(request):
 
     return Response({"isValid": is_valid, "errors": errors}, status=status.HTTP_200_OK)
 
+@api_view(["POST"])
+def substitution(request):
+    user = request.user
+    json_data = request.data
+    proof = get_or_set_induction_proof(user)
+
+    anchor_or_leap = 'anchor' if json_data['isAnchor'] else 'leap'
+    pSide = 'lhs_' if json_data['side'] == 'LHS' else 'rhs_'
+    currentProof = proof[pSide+anchor_or_leap]
+
+    
+
+    currentProof.addProofLine(
+            json_data["currentRacket"], json_data["rule"], json_data["startPosition"], json_data["substitution"]
+        )
+
+    proof['current_proof'] = currentProof
+    proof = update_is_valid(proof)
+    is_valid = proof["isValid"]
+
+    racket_str = (
+        currentProof.getPrevRacket() if is_valid else "Error generating racket"
+    )
+    errors, proof = get_errors_and_clear(proof)
+
+    save_induction_proof_to_cache(user, proof)
+
+    return Response(
+        {"isValid": is_valid, "racket": racket_str, "errors": errors},
+        status=status.HTTP_200_OK,
+    )
+
+@api_view(["POST"])
+def edit_udf(request):
+    pass
+@api_view(["POST"])
+def remove_udf(request):
+    pass
 
 def clear_induction_proof(user):
     cache.delete(f"induction_proof_{user.username}")
