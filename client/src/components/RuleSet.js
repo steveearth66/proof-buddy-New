36dd9a4e9b4ecbc213d96bde1ac9407299481b87
Jerry Huang <jh4248@drexel.edu>
2025-06-04 11:30:46 -0400

    Update frontend rule set

--- client/src/components/RuleSet.js
+++ client/src/components/RuleSet.js
@@ -1,63 +1,148 @@
-const ruleSet = () => { //just creates a ruleset for the ER Page. If new rules are added, they need only be added to this file
+/**
+ * @returns An array of two arrays of rule objects. First array is for eval rules, second is for apply rules.
+ */
+const ruleSet = () => {
+  //just creates a ruleset for the ER Page. If new rules are added, they need only be added to this file
 
   return [
-    {
-      toFrom: "(cons (first L) (rest L))",
-      fromTo: "L",
-      name: "cons",
-      tags: "test"
-    },
-    {
-      toFrom: "(first '(a b ... z))",
-      fromTo: "a",
-      name: "firstList",
-      tags: "first, quote"
-    },
-    { toFrom: "(first (cons x L))", fromTo: "x", name: "first", tags: "test" },
-    {
-      toFrom: "(rest '(a b ... z))",
-      fromTo: "'(b ... z)",
-      name: "restList",
-      tags: "rest, quote"
-    },
-    { toFrom: "(rest (cons x L))", fromTo: "L", name: "rest", tags: "test" },
-    {
-      toFrom: "‘(x y … z)",
-      fromTo: "(cons x ‘(y…z))",
-      name: "consList",
-      tags: "test"
-    },
-    { toFrom: "(if #t x y)", fromTo: "x", name: "if", tags: "test" },
-    { toFrom: "(if #f x y)", fromTo: "y", name: "if", tags: "test" },
-    { toFrom: "(if x y y)", fromTo: "y", name: "if", tags: "test" },
-    { toFrom: "(null? (cons x L))", fromTo: "#f", name: "null?", tags: "test" },
-    {
-      toFrom: "(zero? n)",
-      fromTo: "#t if n is 0, #f if n>0",
-      name: " zero?",
-      tags: "zero, 0"
-    },
-    { toFrom: "(zero? (+ x y))", fromTo: "#f", name: "zero?", tags: "test" },
-    { toFrom: "(cons? (cons x L))", fromTo: "#t", name: "cons?", tags: "test" },
-    {
-      toFrom: "<arithmetic expression>",
-      fromTo: "<integer answer>",
-      name: "math",
-      tags: "+, -, *, quotient, remainder, expt, =, >, <, >=, <="
-    },
-    {
-      toFrom: "<logic expression>",
-      fromTo: "<boolean answer>",
-      name: "logic",
-      tags: "and, or, not, implies, xor"
-    },
-    {
-      toFrom: "<user created label>",
-      fromTo: "<user created definition>",
-      name: "<label>",
-      tags: "definition, label"
-    }
-  ]; 
+    [
+      {
+        procedure: "first",
+        highlight: "(first '(a b c…z))",
+        result: "a"
+      },
+      {
+        procedure: "rest",
+        highlight: "(first '(a b c … z))",
+        result: "'(b c … z)"
+      },
+      {
+        procedure: "cons",
+        highlight: "(cons a '(b c … z))",
+        result: "'(a b c … z)"
+      },
+      {
+        procedure: "if",
+        highlight: "(if #t x y)",
+        result: "x"
+      },
+      {
+        procedure: "if",
+        highlight: "(if #f x y)",
+        result: "y"
+      },
+      {
+        procedure: "if",
+        highlight: "(if x y y)",
+        result: "y"
+      },
+      {
+        procedure: "null?",
+        highlight: "(null? null)",
+        result: "#t"
+      },
+      {
+        procedure: "null?",
+        highlight: "(null? (cons a L))",
+        result: "#f"
+      },
+      {
+        procedure: "zero?",
+        highlight: "(zero? 0)",
+        result: "#t"
+      },
+      {
+        procedure: "zero",
+        highlight: "(zero? n), where n ≠ 0",
+        result: "#f"
+      },
+      {
+        procedure: "and",
+        highlight: "(and p q)",
+        result: "p ∧ q"
+      },
+      {
+        procedure: "or",
+        highlight: "(or p q)",
+        result: "p ∨ q"
+      },
+      {
+        procedure: "not",
+        highlight: "(not p)",
+        result: "¬p"
+      },
+      {
+        procedure: "implies",
+        highlight: "(implies p q)",
+        result: "p → q"
+      },
+      {
+        procedure: "xor",
+        highlight: "(xor p q)",
+        result: "p ⊕ q"
+      },
+      {
+        procedure: "+, -, *, =, <, >, <=, >=",
+        highlight: "(<function> a b)",
+        result: "(a <function> b)"
+      },
+      {
+        procedure: "quotient",
+        highlight: "(quotient a b)",
+        result: "⌊a / b⌋"
+      },
+      {
+        procedure: "remainder",
+        highlight: "(remainder a b)",
+        result: "a % b"
+      },
+      {
+        procedure: "expt",
+        highlight: "(expt a b)",
+        result: <>a<sup>b</sup></>
+      }
+    ],
+    [
+      {
+        name: "cons",
+        procedure: "cons-first-rest",
+        highlight: "(cons (first L) (rest L))",
+        result: "L"
+      },
+      {
+        name: "first",
+        procedure: "first-cons",
+        highlight: "(first (cons x L))",
+        result: "x"
+      },
+      {
+        name: "rest",
+        procedure: "rest-cons",
+        highlight: "(rest (cons x L))",
+        result: "x"
+      },
+      {
+        procedure: "<function name>",
+        highlight: "(<function name> ...)",
+        result: "function definiton with parameters mapped to values"
+      },
+      {
+        procedure: "<proof name>",
+        highlight: "<proof LHS>",
+        result: "<proof RHS>"
+      },
+      {
+        procedure: "IH",
+        highlight: "<IH LHS>",
+        result: "<IH RHS>"
+      },
+      {
+        procedure: "math",
+        highlight: "<mathematical expression>",
+        result: "<equivalent mathematical expression>"
+      }
+    ]
+  ];
 }
 
 export default ruleSet;
