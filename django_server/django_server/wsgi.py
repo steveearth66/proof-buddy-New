f2fba7bdf510a9ea887648db51b4e8b23b7d98a9
Nadeem, Ahsan [JJCUS NON-J&J] <nadeemahsan98@gmail.com>
2026-01-18 18:26:58 -0500

    Add pymysql installation for MySQLdb compatibility in WSGI and manage.py

--- django_server/django_server/wsgi.py
+++ django_server/django_server/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_server.settings')
 
 application = get_wsgi_application()
