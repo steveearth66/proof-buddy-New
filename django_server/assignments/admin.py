50e6bc6b2a1294550c9a4a797c9fc811d491baf5
Steve Earth <se435@drexel.edu>
2026-02-04 03:56:41 -1000

    creating exportable db

--- django_server/assignments/admin.py
+++ django_server/assignments/admin.py
@@ -1,12 +1,38 @@
 from django.contrib import admin
 from .models import Assignment, AssignmentSubmission, Term
+import csv
+from django.http import HttpResponse
 
 # Register your models here.
+
+
+def export_to_csv(modeladmin, request, queryset):
+    """Export selected items to CSV"""
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename={opts.verbose_name_plural}.csv'
+    
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    
+    # Write header
+    writer.writerow([field.verbose_name for field in fields])
+    
+    # Write data rows
+    for obj in queryset:
+        row = [getattr(obj, field.name) for field in fields]
+        writer.writerow(row)
+    
+    return response
+
+export_to_csv.short_description = "Export selected to CSV"
+
 
 class AssignmentAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'due_date', 'term_instructor', 'created_at', 'updated_at', 'created_by')
     search_fields = ('title', 'description')
     readonly_fields = ('created_at', 'updated_at')
+    actions = [export_to_csv]
 
     filter_horizontal = ()
     list_filter = ()
@@ -50,6 +76,7 @@ class AssignmentSubmissionAdmin(admin.ModelAdmin):
     list_display = ('assignment', 'student', 'submission_date', 'grade')
     search_fields = ('assignment', 'student')
     readonly_fields = ('submission_date',)
+    actions = [export_to_csv]
 
     filter_horizontal = ()
     list_filter = ()
@@ -68,6 +95,7 @@ class TermAdmin(admin.ModelAdmin):
     list_display = ('name', 'instructor', 'created_by', 'created_at', 'updated_at')
     search_fields = ("name", "instructor")
     readonly_fields = ('created_at', 'updated_at')
+    actions = [export_to_csv]
 
     filter_horizontal = ()
     list_filter = ()
