123cf87eed3471249b837cd7b845e58c17d183da
Jeetu SK <128751788+JeetuSK0808@users.noreply.github.com>
2026-04-24 17:10:35 -0400

    fix: normalize indentation to 4-space in accounts/models.py

--- django_server/accounts/models.py
+++ django_server/accounts/models.py
@@ -18,34 +18,34 @@ load_dotenv()
 
 class AccountManager(BaseUserManager):
     def create_user(self, email, username, password=None, **other_fields):
-        if not email:
-            raise ValueError("Users must have an email address")
-        if not username:
-            raise ValueError("Users must have a username")
+    if not email:
+        raise ValueError("Users must have an email address")
+    if not username:
+        raise ValueError("Users must have a username")
 
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-            **other_fields,
-        )
+    user = self.model(
+        email=self.normalize_email(email),
+        username=username,
+        **other_fields,
+    )
 
-        user.is_active = True # NOTE: remove after permanent email verification solution is found
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+    user.is_active = True # NOTE: remove after permanent email verification solution is found
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
 
     def create_superuser(self, email, username, password):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            password=password,
-            username=username,
-        )
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.is_active = True
-        user.save(using=self._db)
-        return user
+    user = self.create_user(
+        email=self.normalize_email(email),
+        password=password,
+        username=username,
+    )
+    user.is_admin = True
+    user.is_staff = True
+    user.is_superuser = True
+    user.is_active = True
+    user.save(using=self._db)
+    return user
 
 
 class Account(AbstractBaseUser, PermissionsMixin):
@@ -67,7 +67,7 @@ class Account(AbstractBaseUser, PermissionsMixin):
     objects = AccountManager()
 
     def __str__(self):
-        return self.username
+    return self.username
 
 
 def generate_key():
@@ -81,7 +81,7 @@ class ActivateAccount(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.user.username
+    return self.user.username
 
 
 class ResetPassword(models.Model):
@@ -90,25 +90,25 @@ class ResetPassword(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.user.username
+    return self.user.username
 
 # NOTE: uncomment after permanent email verification solution is found
 """ @receiver(post_save, sender=Account)
 def create_activation_key(sender, instance, created, **kwargs):
     if created and not instance.is_superuser:
-        activation = ActivateAccount.objects.create(user=instance)
-        email = instance.email
-        username = instance.username
-        key = activation.activation_key
-        send_activation_email(email, username, key) """
+    activation = ActivateAccount.objects.create(user=instance)
+    email = instance.email
+    username = instance.username
+    key = activation.activation_key
+    send_activation_email(email, username, key) """
 
 
 def send_activation_email(email, username, key):
     html_content = f"""
-        <p>Hello {username}!</p>
-        <p>Thank you for signing up to Proof Buddy! To get started please confirm your email address by visiting the following link:</p>
-        <a href="{os.getenv('FRONTEND_URL')}/#/verify-success?token={key}" target="_blank">{os.getenv('FRONTEND_URL')}/#/verify-success?token={key}</a>
-        <p>Thank you,<br/>Proof Buddy Team</p>
+    <p>Hello {username}!</p>
+    <p>Thank you for signing up to Proof Buddy! To get started please confirm your email address by visiting the following link:</p>
+    <a href="{os.getenv('FRONTEND_URL')}/#/verify-success?token={key}" target="_blank">{os.getenv('FRONTEND_URL')}/#/verify-success?token={key}</a>
+    <p>Thank you,<br/>Proof Buddy Team</p>
     """
     text_content = ""
     subject = "Proof Buddy - Confirm Your Email"
@@ -122,18 +122,18 @@ def send_activation_email(email, username, key):
 @receiver(post_save, sender=ResetPassword)
 def send_reset_password_email(sender, instance, created, **kwargs):
     if created:
-        print("sending email")
-        email = instance.user.email
-        username = instance.user.username
-        key = instance.reset_key
-        send_reset_email(email, username, key)
+    print("sending email")
+    email = instance.user.email
+    username = instance.user.username
+    key = instance.reset_key
+    send_reset_email(email, username, key)
 
 
 def send_reset_email(email, username, key):
     html_content = f"""
-        <p>Hello {username}!</p>
-        <p>It seems you've forgotten your password. To reset your password please visit the following link:</p>
-        <a href="{os.getenv('FRONTEND_URL')}/#/reset-password?token={key}" target="_blank">{os.getenv('FRONTEND_URL')}/#/reset-password?token={key}</a>
+    <p>Hello {username}!</p>
+    <p>It seems you've forgotten your password. To reset your password please visit the following link:</p>
+    <a href="{os.getenv('FRONTEND_URL')}/#/reset-password?token={key}" target="_blank">{os.getenv('FRONTEND_URL')}/#/reset-password?token={key}</a>
     """
     text_content = ""
     subject = "Proof Buddy - Reset Your Password"
