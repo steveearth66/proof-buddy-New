17dfd3262805104191239f04d3e302d1b2a41d5f
Jeetu SK <128751788+JeetuSK0808@users.noreply.github.com>
2026-04-24 18:27:40 -0400

    Fix indentation in 0014_add_comment_fields migration

--- django_server/induction_api/migrations/0014_add_comment_fields.py
+++ django_server/induction_api/migrations/0014_add_comment_fields.py
@@ -4,24 +4,24 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-      dependencies = [
-                ('induction_api', '0013_inductionproof_support_current_lhs_rhs_and_more'),
-      ]
+    dependencies = [
+        ('induction_api', '0013_inductionproof_support_current_lhs_rhs_and_more'),
+    ]
 
     operations = [
-              migrations.AddField(
-                            model_name='inductionproofline',
-                            name='instructor_comment',
-                            field=models.TextField(blank=True, default=''),
-              ),
-              migrations.AddField(
-                            model_name='inductionproofline',
-                            name='student_comment',
-                            field=models.TextField(blank=True, default=''),
-              ),
-              migrations.AddField(
-                            model_name='inductionproofline',
-                            name='comment_correct',
-                            field=models.BooleanField(default=None, null=True),
-              ),
+        migrations.AddField(
+            model_name='inductionproofline',
+            name='instructor_comment',
+            field=models.TextField(blank=True, default=''),
+        ),
+        migrations.AddField(
+            model_name='inductionproofline',
+            name='student_comment',
+            field=models.TextField(blank=True, default=''),
+        ),
+        migrations.AddField(
+            model_name='inductionproofline',
+            name='comment_correct',
+            field=models.BooleanField(default=None, null=True),
+        ),
     ]
