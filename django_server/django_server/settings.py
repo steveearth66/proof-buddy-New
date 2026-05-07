4f9e2015b9779fde670b52c8bc58325ecafa8c36
Steve Earth <se435@drexel.edu>
2026-05-06 21:27:26 -1000

    cleaned up testing suite

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     "assignments",
 ]
 
+TEST_RUNNER = 'proofs.runner.ColorDiscoverRunner'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
